073fac38d64a7b1c0695f2bd850645910ac5d341
Jeff Brown <jeff@jeffbrown.tech>
2025-03-21 06:39:29 -0700

    Completed Exercise 2 with math operations methods

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Clear();
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite food?");
+            string food = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite book?");
+            string book = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+
+            Console.WriteLine("What is your pet's name?");
+            string pet = Console.ReadLine();
+
+            CreateStory(name, food, book, color, pet);
+        }
+
+        public static void CreateStory (string name, string food, string book, string color, string pet)
+        {
+            Console.WriteLine($"{name} sat under a tree wearing a {color} hoodie.");
+            Console.WriteLine($"{name} shared some {food} with their pet, {pet}.");
+            Console.WriteLine($"{name} flipped open {book} to read another chapter.");
         }
     }
 }
